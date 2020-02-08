8f2b1cad2f436ad03c2472c2122533c2a7b13c55
Saint-John Royce <vzhu6682@uni.sydney.edu.au>
2020-02-08 13:14:24 +0800

    added total price calculation

--- productAccounting.py
+++ productAccounting.py
@@ -1,22 +1,42 @@
 products = []
+totalPrice = 0
 
 while True:
 	name = input('please enter product name: ')
 	if name == 'q':
 		break
-	price = input('and the price of the item? ')
-	
-	p = []
-	p.append(name)
-	p.append(price)
-	
-	# an easier way is to write p = [name, price]
+	price = input('and the price of the item (in integer)? ')
+	priceInt = int(price)
+	itemNumber = input('how many of these would you like: ')
+	itemNumberInt = int(itemNumber)
+	itemTotalPriceInt = priceInt * itemNumberInt
+	totalPrice = totalPrice + itemTotalPriceInt
 
-	products.append(p)
+	#p = []
+	#p.append(name)
+	#p.append(price)
+	#p.append(itemNumber)
+
+	products.append([name, price, itemNumber, str(itemTotalPriceInt)])
 
 	# a further easier way is to write products.append([name, price])
 
-print(products)
+# display methods
+#for p in products:
+#	print(p[0], p[1], p[2], p[3])
 
-for p in products:
-	print(p[0], 'has a price of', p[1])
+# write in a 'file' known as 'product.csv'
+with open('product.csv', 'w') as file:
+
+	file.write('Item name' + ',' + 'Individual Price' + ',' + 'Number of Items' + ',' + 'Total Item Price' + '\n')
+
+	# for every line 'p' in 'products'
+	for p in products:
+
+		# it is written in 'file' the content from each line 'p' 
+		# with the additions
+		file.write(p[0] + ',' + p[1] + ',' + p[2] + ',' + p[3] + '\n')
+
+	file.write(',' + ',' + 'Total Price: ' + ',' + str(totalPrice))
+
+
